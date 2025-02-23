9f0d1c231f3ab5101c88377802878523f3a4d8a4
Oleksandr Klimach <oklimach@deloitte.com>
2025-02-23 14:10:56 +0200

    Lab 3

---
+++ Lab3/Numbers.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3Project
+{
+    abstract class Number
+    {
+        public abstract Number Add(Number other);
+        public abstract Number Subtract(Number other);
+        public abstract Number Multiply(Number other);
+        public abstract Number Divide(Number other);
+
+        public static Number operator +(Number a, Number b) => a.Add(b);
+        public static Number operator -(Number a, Number b) => a.Subtract(b);
+        public static Number operator *(Number a, Number b) => a.Multiply(b);
+        public static Number operator /(Number a, Number b) => a.Divide(b);
+    }
+
+    class Integer(int value) : Number
+    {
+        public int Value { get; } = value;
+
+        public override Number Add(Number other)
+        {
+            return other switch
+            {
+                Integer intNum => new Integer(Value + intNum.Value),
+                Real realNum => new Real(Value + realNum.Value),
+                _ => throw new ArgumentException("Unsupported number type")
+            };
+        }
+
+        public override Number Subtract(Number other)
+        {
+            return other switch
+            {
+                Integer intNum => new Integer(Value - intNum.Value),
+                Real realNum => new Real(Value - realNum.Value),
+                _ => throw new ArgumentException("Unsupported number type")
+            };
+        }
+
+        public override Number Multiply(Number other)
+        {
+            return other switch
+            {
+                Integer intNum => new Integer(Value * intNum.Value),
+                Real realNum => new Real(Value * realNum.Value),
+                _ => throw new ArgumentException("Unsupported number type")
+            };
+        }
+
+        public override Number Divide(Number other)
+        {
+            return other switch
+            {
+                Integer intNum => new Real((double)Value / intNum.Value),
+                Real realNum => new Real(Value / realNum.Value),
+                _ => throw new ArgumentException("Unsupported number type")
+            };
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+
+    class Real(double value) : Number
+    {
+        public double Value { get; } = value;
+
+        public override Number Add(Number other)
+        {
+            return other switch
+            {
+                Integer intNum => new Real(Value + intNum.Value),
+                Real realNum => new Real(Value + realNum.Value),
+                _ => throw new ArgumentException("Unsupported number type")
+            };
+        }
+
+        public override Number Subtract(Number other)
+        {
+            return other switch
+            {
+                Integer intNum => new Real(Value - intNum.Value),
+                Real realNum => new Real(Value - realNum.Value),
+                _ => throw new ArgumentException("Unsupported number type")
+            };
+        }
+
+        public override Number Multiply(Number other)
+        {
+            return other switch
+            {
+                Integer intNum => new Real(Value * intNum.Value),
+                Real realNum => new Real(Value * realNum.Value),
+                _ => throw new ArgumentException("Unsupported number type")
+            };
+        }
+
+        public override Number Divide(Number other)
+        {
+            return other switch
+            {
+                Integer intNum => new Real(Value / intNum.Value),
+                Real realNum => new Real(Value / realNum.Value),
+                _ => throw new ArgumentException("Unsupported number type")
+            };
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Lab3Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new();
+
+            Integer int1 = new(random.Next(1, 100));
+            Integer int2 = new(random.Next(1, 100));
+            Real real1 = new(random.NextDouble() * 100);
+            Real real2 = new(random.NextDouble() * 100);
+
+            Console.WriteLine($"Integer 1: {int1}");
+            Console.WriteLine($"Integer 2: {int2}");
+            Console.WriteLine($"Real 1: {real1}");
+            Console.WriteLine($"Real 2: {real2}");
+
+            PerformOperations(int1, int2, "INT/INT");
+            PerformOperations(int1, real1, "INT/REAL");
+            PerformOperations(real1, int2, "REAL/INT");
+            PerformOperations(real1, real2, "REAL/REAL");
+        }
+
+        static void PerformOperations(Number num1, Number num2, string label)
+        {
+            Number sum = num1 + num2;
+            Number difference = num1 - num2;
+            Number product = num1 * num2;
+            Number fraction = num1 / num2;
+
+            Console.WriteLine($"\n----- {label} -----");
+            Console.WriteLine($"Sum of {num1} and {num2}: {sum}");
+            Console.WriteLine($"Difference of {num1} and {num2}: {difference}");
+            Console.WriteLine($"Product of {num1} and {num2}: {product}");
+            Console.WriteLine($"Fraction of {num1} and {num2}: {fraction}");
+        }
+    }
+}
