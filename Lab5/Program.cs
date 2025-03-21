3c62a1e82ba42a94d4eaf9427f3538dd0e778467
Oleksandr Klimach <Oleksandr.Klimach@trinetix.com>
2025-03-21 18:25:29 +0200

    Lab 5

---
+++ Lab5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Lab5Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type 'Exit' for close programm");
+            while (true)
+            {
+                Console.Write("Input >: ");
+                string? input = Console.ReadLine();
+                if (input != null)
+                {
+                    if (input.Trim().ToLower().Equals("exit"))
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"Output >: {Regex.Replace(input, @"\s+", " ")}");
+                }
+            }
+        }
+    }
+}
