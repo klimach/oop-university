1c2637581862f2b89ee57a1fcfa720b00fe18990
Oleksandr Klimach <Oleksandr.Klimach@trinetix.com>
2025-03-23 21:32:20 +0200

    Lab 6

---
+++ Lab6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Lab6Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string errorReason = string.Empty;
+            try
+            {
+                Console.WriteLine("Enter 3 sides (one-by-one) of Triange to calculate it's Area:");
+                Triangle triangle = RetrieveTriangleFromUser();
+                Console.WriteLine($"Triangle Area =: {triangle.GetTriangleArea()}");
+            }
+            catch (FormatException ex)
+            {
+                errorReason = "Entered number is in incorrect format. Expected examples: '1', '124,5', '12.3'";
+            }
+            catch (ArgumentException ex)
+            {
+                errorReason = ex.Message;
+            }
+            catch (ArithmeticException ex)
+            {
+                errorReason = ex.Message;
+            }
+            catch (Exception ex)
+            {
+                errorReason = $"Unexpected error occured. Please check input values and try again.\n{ex.Message}";
+            }
+
+            if (!string.IsNullOrEmpty(errorReason))
+            {
+                Console.WriteLine(errorReason);
+            }
+        }
+
+        private static Triangle RetrieveTriangleFromUser()
+        {
+            double[] sides = new double[3];
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.Write($"Side {i} = ");
+                string? consoleInput = Console.ReadLine();
+                sides[i - 1] = double.Parse(consoleInput);
+            }
+
+            return new Triangle(sides[0], sides[1], sides[2]);
+        }
+    }
+}
---
+++ Lab6/Triangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6Project
+{
+    public class Triangle(double a, double b, double c)
+    {
+        private readonly double sideA = a;
+        private readonly double sideB = b;
+        private readonly double sideC = c;
+
+        private bool IsTriangleValid()
+        {
+            return sideA > 0 && sideB > 0 && sideC > 0 &&
+                   sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA;
+        }
+
+        public double GetTriangleArea()
+        {
+            if (!IsTriangleValid())
+                throw new ArgumentException("Incorrect sides");
+
+            double p = (sideA + sideB + sideC) / 2;
+            double areaSquared = p * (p - sideA) * (p - sideB) * (p - sideC);
+
+            if (areaSquared < 0)
+                throw new ArithmeticException("Impossible to calculate Area — Squared expression less then 0");
+
+            return Math.Sqrt(areaSquared);
+        }
+    }
+}
